ae946bb96407a75274c93f9234f7b67cdf06ed6c
Pedro Jimenez <pedrojimenezlujan1@gmail.com>
2026-03-30 17:38:49 +0200

    fix a la barra otra vez

--- vite.config.js
+++ vite.config.js
@@ -10,13 +10,4 @@ export default defineConfig({
         }),
         react(),
     ],
-    build: {
-        rollupOptions: {
-            external: [
-                '@tauri-apps/api/window',
-                '@tauri-apps/plugin-updater',
-                '@tauri-apps/plugin-process',
-            ],
-        },
-    },
 });
